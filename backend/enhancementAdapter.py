0e2f7aafd1ac188c10d6c161db74376253c8b740
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-04-12 23:47:02 +0300

    Added adapters for backend tasks

---
+++ backend/enhancementAdapter.py
@@ -0,0 +1,9 @@
+
+#import backend.enhancement as enhancement
+
+
+def add_cats(userName):
+    sourceUserFolder = 'C:\\Users\\frank\\Desktop\\insta_sources\\' + userName + '\\'
+    resultUserFolder = 'C:\\Users\\frank\\Desktop\\catified\\' + userName + '\\'
+
+    #enhancement.add_cats(False, userName, sourceUserFolder, resultUserFolder)
