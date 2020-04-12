132453436c11705c870fcfe8cf0b710a632ca9d0
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-04-12 23:56:10 +0300

    Modified installation scripts

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 pip install virtualenv
 virtualenv venv
 call .\venv\Scripts\activate.bat
+pip install instagram-scraper
+pip install opencv-python
+pip install pillow
 pip install Flask
-pip install Celery
