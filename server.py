c2b1f0354be33d4736234b18a4d233136ed1a89b
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-04-12 23:50:22 +0300

    Refactored server a bit

--- server.py
+++ server.py
@@ -1,24 +1,25 @@
+import sys
 import time
 import serverHelper
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
+
+
+flask_app = Flask(__name__)
 
 
 # falsk uses GET request for every page by default, so there is no need to use it
 # explicitly
-
-
-@app.route('/')
+@flask_app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/<string:page_name>.html')
+@flask_app.route('/<string:page_name>.html')
 def home(page_name=None):
     return render_template(f'{page_name}.html')
 
 
-@app.route('/submit_form')
+@flask_app.route('/submit_form')
 def submit_form():
     if (instagramAccount:= request.args.get('instagramAccount')):
         if not serverHelper.validateInstagramAccount(instagramAccount):
@@ -33,37 +34,36 @@ def submit_form():
         else:
             return redirect(url_for('error'))
     else:
-        print('here')
         return redirect(url_for('processing', instagramAccount=instagramAccount))
 
 
-@app.route('/processing.html')
+@flask_app.route('/processing.html')
 def processing():
     instagramAccount = request.args.get('instagramAccount')
     return render_template('processing.html', instagramAccount=instagramAccount)
 
 
-@app.route('/recognition_ready')
+@flask_app.route('/recognition_ready')
 def recognition_ready():
-    print('recognition')
+    instagramAccount = request.args.get('instagramAccount')
     time.sleep(0.25)
     return '', 200
 
 
-@app.route('/modification_ready')
+@flask_app.route('/modification_ready')
 def modification_ready():
     time.sleep(0.25)
     return '', 200
 
 
-@app.route('/redirect_to_<string:page_name>')
+@flask_app.route('/redirect_to_<string:page_name>')
 def redirect_to(page_name=None):
     time.sleep(0.25)
     data = {'redirect': f'{page_name}.html'}
     return data, 200
 
 
-@app.route('/get_photos')
+@flask_app.route('/get_photos')
 def get_photos():
     photos = {str(key): './static/assets/cat-min.png' for key in range(10)}
     data = {'photos': photos}
