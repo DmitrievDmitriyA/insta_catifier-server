048db0c03a337bb6d0b743c8b456cd54df7ac820
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-04-30 15:12:17 +0300

    Added database adapter

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
-import sys
+import sys, os
+sys.path.append(os.path.abspath('.\\backend'))
 import time
+import datetime
 import serverHelper
 from flask import Flask, render_template, request, redirect, url_for
+from threading import RLock
+from cachetools import cached, TTLCache
+import backend.scrapingAdapter as scrapingAdapter
+import backend.enhancementAdapter as enhancementAdapter
+
+
+lock = RLock()
+
+
+class ExtendedTTLCache(TTLCache):
+    def popitem(self):
+        key, value = super().popitem()
+        print(datetime.datetime.now())
+        with lock:
+            enhancementAdapter.removeResults(key)
+            print('Removed results')
+        return key, value
 
 
 flask_app = Flask(__name__)
+cache = ExtendedTTLCache(maxsize=100, ttl=60) #ttl - time to live in seconds
 
 
 # falsk uses GET request for every page by default, so there is no need to use it
@@ -43,22 +63,25 @@ def processing():
     return render_template('processing.html', instagramAccount=instagramAccount)
 
 
-@flask_app.route('/recognition_ready')
+@flask_app.route('/scraping_ready')
 def recognition_ready():
+    print('/scraping_ready')
     instagramAccount = request.args.get('instagramAccount')
-    time.sleep(0.25)
+    scrapingAdapter.scrapePhotos(instagramAccount)
     return '', 200
 
 
 @flask_app.route('/modification_ready')
 def modification_ready():
-    time.sleep(0.25)
+    print('/modification_ready')
+    instagramAccount = request.args.get('instagramAccount')
+    enhancementAdapter.add_cats(instagramAccount)
     return '', 200
 
 
 @flask_app.route('/redirect_to_<string:page_name>')
 def redirect_to(page_name=None):
-    time.sleep(0.25)
+    time.sleep(1)
     data = {'redirect': f'{page_name}.html'}
     return data, 200
 
