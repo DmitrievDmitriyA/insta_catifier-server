3a45a9dd995b7dfcdc74a78afe1d5c595b96a84f
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-05-03 22:17:13 +0300

    Added LFU caching

---
+++ cache.py
@@ -0,0 +1,34 @@
+import datetime
+from cachetools import cached, LFUCache
+from threading import RLock
+import backend.dataBaseAdapter as dataBaseAdapter
+
+
+# Simple thread-safe caching to speed things up a bit
+# Possible issues:
+# - Cache stores urls which could expire in 7 days (it means that it will be outdated after some time)
+# but there is no logic that checks that
+# At the moment it's not an issue because a relativly small cache size is used (It gets updated before 
+# the issue could appear) 
+
+
+lock = RLock()
+
+
+class ExtendedLFUCache(LFUCache):
+    def __getitem__(self, key):
+        with lock:
+            return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        with lock:
+            super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        with lock:
+            super().__delitem__(key)
+            dataBaseAdapter.remove_results_from_bucket(key)
+
+    def popitem(self):
+        with lock:
+            super().popitem()
