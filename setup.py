3064de58399459bf6a1c10dc24079c8ed499ff26
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-05-06 00:18:35 +0300

    Removed dependency_links
    Only creates issues

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=["Flask", "Flask-Script", "minio", "catifier"],
-    dependency_links=['git+https://github.com/DmitrievDmitriyA/insta_catifier.git@e11c9e22548fedd3504cd24d9d765a10316cc47f']
     package_data={
         "catifier-server": ["statis/*", "templates/*"],
     }
