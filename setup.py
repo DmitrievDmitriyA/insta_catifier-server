9d77b7c89dedfc0f0fa4ba1ac1349c970a5ba5e3
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-05-06 00:16:47 +0300

    Fixed setup issues

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="catifier-server",
     version="0.1",
-    packages=find_packages(),,
-    install_requires=["catifier", "Flask", "Flask-Script", "minio"],
+    packages=find_packages(),
+    install_requires=["Flask", "Flask-Script", "minio", "catifier"],
     dependency_links=['git+https://github.com/DmitrievDmitriyA/insta_catifier.git@e11c9e22548fedd3504cd24d9d765a10316cc47f']
     package_data={
-        "catifier": ["statis/*", "templates/*"],
+        "catifier-server": ["statis/*", "templates/*"],
     }
 )
