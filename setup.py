c6238793afc67e78b36107498d84048b6be9eb26
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2020-05-07 01:16:43 +0300

    Added forgotten cachetools to requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="catifier-server",
     version="0.1",
     packages=find_packages(),
-    install_requires=["Flask", "Flask-Script", "minio", "catifier"],
+    install_requires=["Flask", "Flask-Script", "minio", "catifier", "cachetools"],
     package_data={
         "catifier-server": ["statis/*", "templates/*"],
     }
